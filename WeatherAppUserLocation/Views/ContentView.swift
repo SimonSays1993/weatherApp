2109186d4a8d7fa3928f515825f36fa88f1bb8e1
Simon McNeil <simonmcneil062@gmail.com>
2022-01-29 13:21:00 -0500

    Create a SwiftUI project for Weather info based off user location

--- WeatherAppUserLocation/Views/ContentView.swift
+++ WeatherAppUserLocation/Views/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    @State var weather: ResponseBody?
+    
+    var weatherManager = WeatherManager()
+    
     
     var body: some View {
         VStack {
             if let location = locationManager.location {
-                Text("You coordinates are: \(location.longitude), \(location.latitude)")
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                               weather = try await weatherManager.getCurrentWeather(latitue: location.latitude, longtitue: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
