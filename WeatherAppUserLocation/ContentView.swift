b96af740b4d933d6b27a918a4b9d59557b523acd
Simon McNeil <simonmcneil062@gmail.com>
2022-01-28 21:39:38 -0500

    Initial Commit

---
+++ WeatherAppUserLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAppUserLocation
+//
+//  Created by Simon Mcneil on 2022-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAppUserLocation/WeatherAppUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUserLocationApp.swift
+//  WeatherAppUserLocation
+//
+//  Created by Simon Mcneil on 2022-01-28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
